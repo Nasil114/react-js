26893ea2b4f6fdf2ed6601d92e2948c04c5b0215
Nasil114 <120000998+Nasil114@users.noreply.github.com>
2024-02-17 18:38:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import DropdownExample from './DropdownExample';
+import './App.css'; // Import the CSS file
+
+function App() {
+  return (
+    <div className="page">
+      <div className="content">
+        <DropdownExample />
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App;
